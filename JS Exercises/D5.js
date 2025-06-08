d636141bb65d8e585d8559cc756a1b877954169b
Alfonso Sarno <alfonsosarno1@icloud.com>
2025-06-08 16:14:00 +0200

    adding content

--- JS Exercises/D5.js
+++ JS Exercises/D5.js
@@ -24,10 +24,14 @@ console.log(pets);
     Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
 */
 pets.sort().reverse();
+
 console.log(pets);
+
 /* ESERCIZIO 4
     Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
-*/
+// */ const x = pets.splice(0, 1);
+pets.push(x);
+console.log(pets);
 
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
@@ -52,11 +56,27 @@ const cars = [
     trims: ["life", "style", "r-line"],
   },
 ];
+for (let i = 0; i < cars.length; i++) {
+  cars[i].licensePlate = "abc";
+}
+console.log(cars);
 
 /* ESERCIZIO 6
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
 */
+
+cars.push({
+  brand: "audi",
+  model: "A1",
+  color: "black",
+  trims: ["base", "sport"],
+  licensePlate: "abc",
+});
+for (let i = 0; i < cars.length; i++) {
+  cars[i].trims.pop();
+}
+console.log(cars);
 
 /* ESERCIZIO 7
     Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
